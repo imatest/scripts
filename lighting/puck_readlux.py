e6bbb3d68eaedf2da4494c58d47dc11879315c28
Henry Koren <h@kmz.me>
2018-08-02 10:43:09 -0600

    Isolight Puck python control scripts

---
+++ lighting/puck_readlux.py
@@ -0,0 +1,19 @@
+import serial
+import time
+
+
+delay = int(raw_input('delay between measurments: '))
+
+baud = 115200
+
+portR = '/dev/ttyUSB3'
+
+
+
+puck1 = serial.Serial(portR, baud)
+
+while True:
+    puck1.write('GRL<LF>\r')
+    time.sleep(delay)
+    msg1 = puck1.readline()
+    print msg1.split(' ')[1].lstrip('0').rstrip('\n')
